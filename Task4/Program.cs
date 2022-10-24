0330aa62f34b81ec699a51445491cca494d8476e
Svetlana <korneevdenis.ru@mail.ru>
2022-10-24 22:01:47 +0300

    Задание четыре

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter the number 1:");
+int number_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the number 2:");
+int number_2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the number 3:");
+int number_3 = int.Parse(Console.ReadLine()!);
+int max = number_1;
+
+if (number_2 > number_1)
+{
+     max = number_2;
+}
+if (number_3 > max) 
+{
+    max = number_3; 
+}
+
+Console.WriteLine(max);
