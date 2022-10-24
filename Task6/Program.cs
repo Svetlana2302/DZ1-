2825cbf505e9e3c712771367bc93c37871641603
Svetlana <korneevdenis.ru@mail.ru>
2022-10-24 22:03:13 +0300

    Задание шесть

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter the number :");
+int number_1 = int.Parse(Console.ReadLine()!);
+
+
+if (number_1%2 == 0)
+{
+     Console.WriteLine("yes");
+}
+
+else 
+{
+    Console.WriteLine("no");
+}
+
