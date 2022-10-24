9e0af3e8f2296c20e99ce4894a5bcd8fa8c9317c
Svetlana <korneevdenis.ru@mail.ru>
2022-10-24 21:59:39 +0300

    Задание один

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter the number 1:");
+int number_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the number 2:");
+int number_2 = int.Parse(Console.ReadLine()!);
+
+if (number_1 > number_2)
+{
+    Console.WriteLine(number_1);
+}
+else {
+    if (number_2 > number_1) {
+        Console.WriteLine(number_2);
+    }
+    }
